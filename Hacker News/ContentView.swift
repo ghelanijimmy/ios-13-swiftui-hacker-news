3af0d408dc36f121a5741c66f01f75f40c8ef08f
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-07-21 07:58:57 -0400

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Jimmy Ghelani on 2020-07-21.
+//  Copyright © 2020 JGDesign Solutions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
